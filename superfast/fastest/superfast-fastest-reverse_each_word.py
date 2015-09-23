30a360183a14ffea7c94b44d9bde0bae5272861c
Wesley Baugh <wesley@bwbaugh.com>
2015-09-22 23:17:15 -0700

    Another variation of reverse each word

--- superfast/fastest/superfast-fastest-reverse_each_word.py
+++ superfast/fastest/superfast-fastest-reverse_each_word.py
@@ -1,1 +1,8 @@
+xs = raw_input()
 print(''.join(list(x)[::-1]) for x in xs)
+
+
+#######################################################################
+
+
+print ' '.join(''.join(reversed(x)) for x in raw_input().split())
