853c1b40d957c1914593c349807efcacf338930c
Wesley Baugh <wesley@bwbaugh.com>
2015-09-25 22:59:28 -0700

    Incomplete: Count plus shapes

---
+++ coffeebreak/fastest/coffeebreak-fastest-count_plus_shapes.py
@@ -0,0 +1,48 @@
+"""
+Statement
+The program:
+Your program is given a grid made of dots .s and Xs. You must find how many times the Xs make a + shape within the grid.
+
+For example, this grid: 
+.X.
+XXX
+.X. has 1 + shaped pattern.
+
+Each X character can be part of several + shapes. For example, this grid:
+XXXX
+XXXX
+XXXX contains a cross here: XXXX
+XXXX
+XXXX and here: XXXX
+XXXX
+XXXX which makes a total of 2.
+
+INPUT:
+Line 1: 2 space separated integers width and height.
+Next height lines: A string of width characters, one row of the grid.
+
+OUTPUT:
+An integer for the number of + patterns composed of Xs in the grid.
+
+CONSTRAINTS:
+0 < width, height < 100
+
+EXAMPLE:
+Input
+5 4
+...X.
+..XXX
+.XXX.
+..XX.
+Output
+2
+ 
+
+Download the files provided in the test script:
+Example: in.txt out.txt
+Alone: in.txt out.txt
+Neighbors: in.txt out.txt
+Square: in.txt out.txt
+Mixed: in.txt out.txt
+"""
+# Wasn't able to complete this one in time.
