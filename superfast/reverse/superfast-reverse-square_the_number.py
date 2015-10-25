c910dc58c06f5cd8b223c1d977e1ebf04f4bccbd
Wesley Baugh <wesley@bwbaugh.com>
2015-10-25 15:32:23 -0700

    Square the given number
    Too easy/

---
+++ superfast/reverse/superfast-reverse-square_the_number.py
@@ -0,0 +1,34 @@
+"""
+The game mode is: REVERSE. You do not have access to the statement.
+Test 1 -
+Provided Input
+3
+Expected Output
+9
+Test 2 -
+Provided Input
+5
+Expected Output
+25
+Test 3 -
+Provided Input
+-3
+Expected Output
+9
+Test 4 -
+Provided Input
+2
+Expected Output
+4
+Test 5 -
+Provided Input
+-10
+Expected Output
+100
+Test 6 -
+Provided Input
+4
+Expected Output
+16
+"""
+print int(raw_input()) ** 2
