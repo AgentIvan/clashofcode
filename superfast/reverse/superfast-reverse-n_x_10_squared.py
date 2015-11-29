74ac8a69dafb530081581d68b7c293f144c678bd
Wesley Baugh <wesley@bwbaugh.com>
2015-11-29 14:17:31 -0800

    (n * 10) ** 2

---
+++ superfast/reverse/superfast-reverse-n_x_10_squared.py
@@ -0,0 +1,30 @@
+"""
+The game mode is: REVERSE. You do not have access to the statement.
+Test 1 -
+Provided Input
+6
+Expected Output
+3600
+Test 2 -
+Provided Input
+12
+Expected Output
+14400
+Test 3 -
+Provided Input
+20
+Expected Output
+40000
+Test 4 -
+Provided Input
+1
+Expected Output
+100
+Test 5 -
+Provided Input
+9
+Expected Output
+8100
+"""
+n = int(input())
+print((n * 10) ** 2)
