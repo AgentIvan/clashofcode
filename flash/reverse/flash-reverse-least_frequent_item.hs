d0315cb8b2d102da3b19f3537f2ecf1b326ba644
Wesley Baugh <wesley@bwbaugh.com>
2015-12-19 23:15:20 -0800

    Find the least frequent number

---
+++ flash/reverse/flash-reverse-least_frequent_item.hs
@@ -0,0 +1,38 @@
+{-
+The game mode is REVERSE: You do not have access to the statement. You have to guess what to do by observing the following set of tests:
+01 Test 1
+5
+2 4 2 4 1
+1
+02 Test 2
+7
+1 1 2 2 3 4 3
+4
+03 Test 3
+1
+9
+9
+04 Test 4
+3
+1 2 1
+2
+05 Test 5
+9
+1 3 2 4 5 4 2 3 1
+5
+-}
+import Control.Arrow
+import Data.List
+
+main :: IO ()
+main = do
+    _ <- getLine
+    input_line <- getLine
+    let xs = map read $ words input_line :: [Int]
+    print $ leastFrequent xs
+
+leastFrequent :: [Int] -> Int
+leastFrequent = snd . head . sort . frequency
+
+frequency :: Ord a => [a] -> [(Int, a)]
+frequency = map (length &&& head) . group . sort
