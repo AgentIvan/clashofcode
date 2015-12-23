39aafa22940dc1b163250df7d8bb26ed21e3a021
Wesley Baugh <wesley@bwbaugh.com>
2015-12-22 22:35:09 -0800

    Rotate string until original string remains

---
+++ flash/shortest/flash-shortest-rotate_string.hs
@@ -0,0 +1,35 @@
+{-
+Given a string L (of length N), print out every right rotation of L until it reverts to the original string.
+
+INPUT:
+Line 1: A string L
+
+OUTPUT:
+Line 1: L
+Line 2: L rotated by one character to the right
+Line 3: L rotated by two characters to the right
+...
+Line N: L rotated by N - 1 characters to the right
+Line N + 1: L
+
+CONSTRAINTS:
+2 ≤ N ≤ 50
+
+EXAMPLE:
+Input
+Codingame
+Output
+Codingame
+eCodingam
+meCodinga
+ameCoding
+gameCodin
+ngameCodi
+ingameCod
+dingameCo
+odingameC
+Codingame
+-}
+import Data.List
+main=interact r
+r x=unlines$x:(nub$reverse$zipWith(++)(tails x)(inits x))
