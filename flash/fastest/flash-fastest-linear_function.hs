7883da8c2ac0982c7bbdc6e9dcda474b3fa92654
Wesley Baugh <wesley@bwbaugh.com>
2015-12-24 14:41:59 -0800

    Compute the linear function at the given points
    https://www.codingame.com/clashofcode/clash/report/758625b8d7f894a6879a8dd42750e8aad0bfd

---
+++ flash/fastest/flash-fastest-linear_function.hs
@@ -0,0 +1,39 @@
+{-
+Your program must compute the value of a linear function at the given points.
+
+A linear function is of the form f(x) = a * x + b, with a and b, two constant values. You are given the values a, b and a set of N values of x. You must compute the value of f(x) for each of the N values of x.
+
+INPUT:
+Line 1 : two space-separated integers a and b
+Line 2 : an integer N
+N next lines : an integer x on each line
+
+OUTPUT:
+N lines : the value of f(x) for each x value given as input
+
+CONSTRAINTS:
+-100 < a, b < 100
+-100 < x < 100
+
+EXAMPLE:
+Input
+5 -2
+3
+3
+8
+-1
+-}
+import System.IO
+import Control.Monad
+
+main :: IO ()
+main = do
+    input_line <- getLine
+    let [a, b] = map read $ words input_line :: [Int]
+    n <- readLn :: IO Int
+    
+    replicateM_ n $ do
+        x <- readLn :: IO Int
+        print $ answer a b x
+
+answer a b x = a * x + b
