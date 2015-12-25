fa491fadbab25e0a86cd54f827dd35531fb0ff28
Wesley Baugh <wesley@bwbaugh.com>
2015-12-24 16:26:37 -0800

    Reverse variant of digital root
    https://www.codingame.com/clashofcode/clash/report/7589047c52990f45ac662db9490785108f2fc

---
+++ flash/reverse/flash-reverse-digital_root.hs
@@ -0,0 +1,29 @@
+{-
+The game mode is REVERSE: You do not have access to the statement. You have to guess what to do by observing the following set of tests:
+01 Test 1
+12
+3
+02 Test 2
+19
+1
+03 Test 3
+17
+8
+04 Test 4
+71
+8
+05 Test 5
+4294967292
+9
+06 Test 6
+95018375
+2
+07 Test 7
+1850
+5
+08 Test 8
+7645
+4
+-}
+
+-- See `flash/shortest/flash-shortest-digital_root.hs`
