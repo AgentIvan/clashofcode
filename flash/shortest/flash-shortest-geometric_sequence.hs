77712d925a893d93ff12e2f43245b6f113ca033f
Wesley Baugh <wesley@bwbaugh.com>
2015-12-24 21:17:15 -0800

    Geometric sequence
    I forgot that this was a shortest-clash, not fastest!
    
    https://www.codingame.com/clashofcode/clash/report/75985dc1a9d392e1d26ddc7dca10bb563d055

---
+++ flash/shortest/flash-shortest-geometric_sequence.hs
@@ -0,0 +1,31 @@
+{-
+A geometric sequence, is a sequence of numbers where each term after the first is found by multiplying the previous one by a number called the common ratio.
+
+Your program must print the N first numbers of a geometric sequence of ratio R and started by 1.
+
+INPUT:
+Two space separated integers N and R.
+
+OUTPUT:
+The first N integers of the geometric sequence of common ratio R, starting with 1.
+
+CONSTRAINTS:
+0 < N < 50
+0 ≤ R ≤ 50
+Each integer of the suite can hold in a 64 bits integer.
+
+EXAMPLE:
+Input
+5 2
+Output
+1 2 4 8 16
+-}
+import System.IO
+import Control.Monad
+
+main :: IO ()
+main = do
+    [n, r] <- fmap (map read . words) getLine :: IO [Int]
+    putStrLn $ unwords $ map show $ geometricSequence n r
+
+geometricSequence n r = take n $ map (r^) [0..]
