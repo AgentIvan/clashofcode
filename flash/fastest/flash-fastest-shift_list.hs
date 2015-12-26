1af5fc7a216960fcc49fa17bcf478b76c5e1bb94
Wesley Baugh <wesley@bwbaugh.com>
2015-12-25 19:02:34 -0800

    Clean up auto generated code

--- flash/fastest/flash-fastest-shift_list.hs
+++ flash/fastest/flash-fastest-shift_list.hs
@@ -16,23 +16,11 @@ Input
 5
 breakfast
 -}
-import System.IO
-import Control.Monad
 
 main :: IO ()
 main = do
-    hSetBuffering stdout NoBuffering -- DO NOT REMOVE
-    
-    -- Auto-generated code below aims at helping you parse
-    -- the standard input according to the problem statement.
-    
-    input_line <- getLine
-    let n = read input_line :: Int
+    n <- readLn :: IO Int
     word <- getLine
-    
-    -- hPutStrLn stderr "Debug messages..."
-    
-    -- Write answer to stdout
     putStrLn $ shift n word
 
 shift :: Int -> String -> String
