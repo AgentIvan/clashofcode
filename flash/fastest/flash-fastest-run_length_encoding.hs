24df517488cede5caf8bc65273e658ea85950c6c
Wesley Baugh <wesley@bwbaugh.com>
2015-12-26 20:00:38 -0800

    Run length encoding
    https://www.codingame.com/clashofcode/clash/report/76747054a6563a400843179ab6ca015bd2cac

---
+++ flash/fastest/flash-fastest-run_length_encoding.hs
@@ -0,0 +1,26 @@
+{-
+You must apply run length encoding algorithm to the given string.
+
+Run length encoding is a simple form of data compression, where sequences of repeating characters are replaced by a pair N, C where N is the size of the sequence and C is the repeated character.
+
+INPUT:
+Line 1: A string S to encode.
+
+OUTPUT:
+Line 1: The run length encoded version of S.
+
+CONSTRAINTS:
+0 < S < 50
+
+EXAMPLE:
+Input
+abbcccdddd
+Output
+1a2b3c4d
+-}
+import Control.Arrow
+import Data.List
+
+main = interact answer
+
+answer xs = concat . concat $ map (\(a, b) -> [show a, [b]]) $ map (length &&& head) $ group xs
