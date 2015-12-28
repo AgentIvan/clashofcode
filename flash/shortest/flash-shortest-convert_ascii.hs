231201d74b5f9dfcd640e7daebc9ea72051b6dd1
Wesley Baugh <wesley@bwbaugh.com>
2015-12-27 19:48:30 -0800

    Convert ordinals to characters
    https://www.codingame.com/clashofcode/clash/report/77269b0acfb39a7bbf79c2b64d42ec71d4815

---
+++ flash/shortest/flash-shortest-convert_ascii.hs
@@ -0,0 +1,23 @@
+{-
+Your program must convert a sequence of integers into a string of ASCII characters.
+
+INPUT:
+Line 1: An integer charCount for the number of integers to convert.
+Line 2: charCount integers charCode, separated by spaces.
+
+OUTPUT:
+A single line containing a string of characters with the given ASCII codes.
+
+CONSTRAINTS:
+0 < charCount < 1000 
+32 ≤ charCode ≤ 126
+
+EXAMPLE:
+Input
+11 
+72 101 108 108 111 32 87 111 114 108 100
+Output
+Hello World
+-}
+import Data.Char
+main=interact$map(chr.read).drop 1.words
