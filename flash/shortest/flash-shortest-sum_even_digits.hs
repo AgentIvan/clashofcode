10637c208876f34ac188c4bd8f05f922867c2bc6
Wesley Baugh <wesley@bwbaugh.com>
2015-12-28 17:58:49 -0800

    Sum the even digits
    https://www.codingame.com/clashofcode/clash/report/777027fdd0f305b0f234d781c2782a1b90851

---
+++ flash/shortest/flash-shortest-sum_even_digits.hs
@@ -0,0 +1,22 @@
+{-
+Your program must calculate the sum of even digits in a number N.
+
+As an example, for 120789466, the result is 2+8+4+6+6=26.
+
+INPUT:
+An integer number N
+
+OUTPUT:
+An integer number, the sum of even digits in N.
+
+CONSTRAINTS:
+0 ≤ N < 2^32
+
+EXAMPLE:
+Input
+120789466
+Output
+26
+-}
+import Data.Char
+main=interact(show.sum.filter even.map digitToInt)
