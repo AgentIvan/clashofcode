67536073abacbe6e9e5853e812c48ced63c4a3e3
Wesley Baugh <wesley@bwbaugh.com>
2016-01-17 16:38:31 -0800

    Title case but keep spaces
    https://www.codingame.com/clashofcode/clash/report/8721377f51e24c4ba267a57751ded93551197

---
+++ flash/reverse/flash-reverse-title_case_keep_spaces.hs
@@ -0,0 +1,24 @@
+{-
+The game mode is REVERSE: You do not have access to the statement. You have to guess what to do by observing the following set of tests:
+01 Test 1
+hello world
+Hello World
+02 Test 2
+ABCDEF
+Abcdef
+03 Test 3
+there is no spoon
+There Is No Spoon
+04 Test 4
+hello   world
+Hello   World
+05 Test 5
+HaVe fUN And KEEP cOding
+Have Fun And Keep Coding
+-}
+import Data.Char
+import Data.List.Split
+
+main = interact $ concatMap firstUpper . split (keepDelimsR $ oneOf " ")
+
+firstUpper (x:xs) = toUpper x : map toLower xs
