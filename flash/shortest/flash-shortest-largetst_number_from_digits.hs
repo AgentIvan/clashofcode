6cabccb24379062757796f07e5daabc8e940a0f5
Wesley Baugh <wesley@bwbaugh.com>
2016-01-18 16:32:34 -0800

    Largest number possible from digits
    https://www.codingame.com/clashofcode/clash/report/8769395c44e644e9124e13d17c417ab4e9ceb

---
+++ flash/shortest/flash-shortest-largetst_number_from_digits.hs
@@ -0,0 +1,23 @@
+{-
+Your program must output the biggest number that is writtable by using the given digits.
+
+INPUT:
+Line 1: An integer N, the number of digits.
+Line 2: N space separated digits.
+
+OUTPUT:
+Line 1: The biggest number that is writtable using all the given digits.
+
+CONSTRAINTS:
+1 ≤ N ≤ 10
+
+EXAMPLE:
+Input
+9
+1 2 3 4 5 6 7 8 9
+Output
+987654321
+-}
+import Data.List
+main=interact$t.concat.reverse.sort.tail.words
+t x|all(=='0') x="0"|True=x
