b8ae0a6787cd613e8217ef22e3bbd7511530462d
Wesley Baugh <wesley@bwbaugh.com>
2016-01-22 20:48:16 -0800

    Simpler implementation for counting letters
    https://www.codingame.com/clashofcode/clash/report/898777882750b5859558d5a6a4345e3850966

--- flash/fastest/flash-fastest-count_letters.hs
+++ flash/fastest/flash-fastest-count_letters.hs
@@ -1,5 +1,18 @@
+{-
+Your program must output the number of letters inside a message. A letter is a un character in [a-z] or [A-Z].
+
+INPUT:
+Line 1: A string message.
+
+OUTPUT:
+Line 1: The number of letters inside the message.
+
+EXAMPLE:
+Input
+I am
+Output
+3
+-}
 import Data.Char
 
-main = interact answer
-
-answer = show . length . filter isAlpha
+main = interact $ show . length . filter isAlpha
