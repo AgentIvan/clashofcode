294f7a0189a644d7c8dd8dfe8765f9b03d6fa0dd
Wesley Baugh <wesley@bwbaugh.com>
2016-01-30 11:31:29 -0800

    Hexadecimal to decimal
    https://www.codingame.com/clashofcode/clash/report/93972a265551c6dbaa86e7ca4ff6f0a67241f

---
+++ flash/shortest/flash-shortest-hexadecimal_to_decimal.hs
@@ -0,0 +1,20 @@
+{-
+Convert a hexadecimal number into decimal.
+
+INPUT:
+A string number which contains a hexadecimal number in capitals.
+
+OUTPUT:
+The decimal value of the given number.
+
+CONSTRAINTS:
+number represents a positive integer ≤ 10000000.
+
+EXAMPLE:
+Input
+F
+Output
+15
+-}
+import Numeric
+main=interact$show.fst.head.readHex
