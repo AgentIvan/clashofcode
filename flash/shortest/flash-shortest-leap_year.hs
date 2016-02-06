794517d85a56832403bda7e595e69c5d4df3d6dc
Wesley Baugh <wesley@bwbaugh.com>
2016-02-05 22:28:00 -0800

    Leap year
    Thought it was a fastest clash since people were submitting. Turns out
    it was a shortest!
    
    https://www.codingame.com/clashofcode/clash/report/9840787060154dfc38adb694cff65f68edda4

---
+++ flash/shortest/flash-shortest-leap_year.hs
@@ -0,0 +1,35 @@
+{-
+Your program must output whether the year given on the standard input is a leap year (true) or not (false).
+To know if a year is a leap year, follow this guide:
+If the year is divisible by 4, go to step 2, otherwise go to step 5.
+If the year is divisible by 100, go to step 3. Otherwise, go to step 4.
+If the year is divisible by 400, go to step 4. Otherwise, go to step 5.
+The year is a leap year (it has 366 days).
+The year is not a leap year (it has 365 days).
+
+INPUT:
+year an integer
+
+OUTPUT:
+true if year is a leap year, false otherwise.
+
+CONSTRAINTS:
+0 < year < 10000
+
+EXAMPLE:
+Input
+2000
+Output
+true
+-}
+import Data.Char
+
+main :: IO ()
+main = interact $ map toLower . show . isLeapYear . read
+
+isLeapYear :: Int -> Bool
+isLeapYear x
+    | x `rem` 4 > 0 = False
+    | x `rem` 100 > 0 = True
+    | x `rem` 400 == 0 = True
+    | otherwise = False
