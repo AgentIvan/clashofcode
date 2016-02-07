753c51687c9b535ad63502799c1448dfdea553b2
Wesley Baugh <wesley@bwbaugh.com>
2016-02-06 23:08:52 -0800

    Shorter molecule name from diagram
    Written from scratch.
    
    https://www.codingame.com/clashofcode/clash/report/990163c892c37b35a02c081ed8c1018e27f54

--- flash/shortest/flash-shortest-molecule_name_from_diagram.hs
+++ flash/shortest/flash-shortest-molecule_name_from_diagram.hs
@@ -1,3 +1,40 @@
-import Data.Maybe
-main=interact a
-a s=fromMaybe"NONE"$lookup(f 'C',f 'H')[((1,4),"methane"),((2,6),"ethane"),((3,8),"propane"),((4,10),"butane"),((5,12),"pentane")]where f x=length$filter(==x)$drop 1 s
+{-
+In chemistry, simple alkanes are made of carbon and hydrogen atoms. According to the IUPAC nomenclature system, they are named by the number of carbons they contain (here are the first 5 alkanes):
+
+- methane CH4 (1 Carbon atom, 4 Hydrogen atoms)
+- ethane C2H6 (2 Carbon atoms , 6 Hydrogen atoms)
+- propane C3H8 (3 Carbon atoms, 8 Hydrogen atoms)
+- butane C4H10 (4 Carbon atoms, 10 Hydrogen atoms)
+- pentane C5H12 (5 Carbon atoms, 12 Hydrogen atoms)
+
+Given a diagram of a molecule find its nomenclature (its IUPAC name).
+
+INPUT:
+Line 1: An integer N for the number of lines used to draw the diagram of a molecule.
+Next N lines: A diagram of a molecule consisting of characters H and C for the element hydrogen and carbon respectively; - and | to represent the bonds.
+
+OUTPUT:
+A single line containing NONE if the given molecule is not an alkane. Otherwise, output the IUPAC name of the given molecule (methane, ethane, ...).
+
+CONSTRAINTS:
+0 ≤ N ≤ 15
+
+EXAMPLE:
+Input
+5
+  H
+  |
+H-C-H
+  |
+  H
+Output
+methane
+-}
+main=interact$a
+a d=n c h where c=length$filter(=='C')d;h=length$filter(=='H')d
+n 1 4="methane"
+n 2 6="ethane"
+n 3 8="propane"
+n 4 10="butane"
+n 5 12="pentane"
+n _ _="NONE"
