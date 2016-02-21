3122875a5c8f2fb97d38a7073eb73515be51844e
Wesley Baugh <wesley@bwbaugh.com>
2016-02-21 11:27:07 -0800

    Most consecutive zeros
    At the time of submission, I didn't realize that only groups of zeros
    should be considered since I didn't let all the tests run.
    
    https://www.codingame.com/clashofcode/clash/report/108024accb688e6fa1dc85ce4d58142cff352f

---
+++ flash/reverse/flash-reverse-most_consecutive_zeros.hs
@@ -0,0 +1,27 @@
+{-
+The game mode is REVERSE: You do not have access to the statement. You have to guess what to do by observing the following set of tests:
+01 Test 1
+100020300001
+4
+02 Test 2
+1000000000
+9
+03 Test 3
+0
+1
+04 Test 4
+123456789
+0
+05 Test 5
+10000000000000000
+16
+06 Test 6
+1020304050
+1
+07 Test 7
+1000600007000000
+6
+-}
+import Data.List
+
+main = interact $ show . maximum . map length . group
