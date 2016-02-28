1b369e0f44a35bcc1e6e6dea86a2f754e15947ea
Wesley Baugh <wesley@bwbaugh.com>
2016-02-27 22:23:30 -0800

    Finish implementation for community puzzle

--- reflection/fastest/reflection-fastest-castle_siege.py
+++ reflection/fastest/reflection-fastest-castle_siege.py
@@ -40,6 +40,9 @@ Input
 ...3..
 ......
 """
+import sys
+
+
 class EnemyWinError(Exception):
     pass
 
@@ -47,6 +50,7 @@ class EnemyWinError(Exception):
 def main():
     w, h = [int(i) for i in input().split()]
     grid = [list(input()) for _ in range(h)]
+    print('\n'.join(''.join(row) for row in grid), file=sys.stderr)
     result = play(grid)
     print(' '.join(str(x) for x in result))
 
@@ -54,28 +58,53 @@ def main():
 def play(grid):
     result = 'WIN'
     num_rounds = 0
-    while not all(x in '.T' for row in grid for x in row):
+    collision = False
+    while not all(x in '.T' for row in grid for x in row) or collision:
         num_rounds += 1
         targets = get_tower_targets(grid)
         grid = remove_hp(grid, targets)
         try:
-            grid = move_enemies(grid)
+            (grid, collision) = move_enemies(grid)
         except EnemyWinError:
             result = 'LOSE'
+            return (result, num_rounds)
+        print('-' * len(grid[0]), file=sys.stderr)
+        print('\n'.join(''.join(row) for row in grid), file=sys.stderr)
+        print(collision, file=sys.stderr)
     return (result, num_rounds)
 
 
 def get_tower_targets(grid):
-    # TODO: Needs implementation.
-    return []
+    for tower in _get_towers(grid):
+        target = _get_tower_target(grid, tower)
+        if not target:
+            continue
+        yield target
 
 
-def get_towers(grid):
+def _get_towers(grid):
     for row_idx, row in enumerate(grid):
         for col_idx, cell in enumerate(row):
             if cell != 'T':
                 continue
             yield (row_idx, col_idx)
+
+
+def _get_tower_target(grid, tower):
+    targets = list(get_enemies(grid))
+    targets = [target for target in targets if _within_5x5(tower, target)]
+    if not targets:
+        return []
+    if len(targets) == 1:
+        return targets[0]
+    targets = _filter_most_north(targets)
+    if len(targets) == 1:
+        return targets[0]
+    targets = _filter_closest(tower, targets)
+    if len(targets) == 1:
+        return targets[0]
+    targets = _filter_furthest_east(targets)
+    return targets[0]
 
 
 def get_enemies(grid):
@@ -86,15 +115,61 @@ def get_enemies(grid):
             yield (row_idx, col_idx)
 
 
+def _within_5x5(tower, target):
+    tower_row, tower_col = tower
+    target_row, target_col = target
+    return bool(
+        (tower_row - 2 <= target_row <= tower_row + 2) and
+        (tower_col - 2 <= target_col <= tower_col + 2)
+    )
+
+
+def _filter_most_north(targets):
+    min_row_idx = min(row_idx for (row_idx, col_idx) in targets)
+    return [
+        (row_idx, col_idx)
+        for (row_idx, col_idx) in targets
+        if row_idx == min_row_idx
+    ]
+
+
+def _filter_closest(tower, targets):
+    min_distance = min(_distance(tower, target) for target in targets)
+    return [
+        target
+        for target in targets
+        if _distance(tower, target) == min_distance
+    ]
+
+
+def _distance(tower, target):
+    tower_row, tower_col = tower
+    target_row, target_col = target
+    return abs(tower_row - target_row) + abs(tower_col - target_col)
+
+
+def _filter_furthest_east(targets):
+    max_col_idx = max(col_idx for (row_idx, col_idx) in targets)
+    return [
+        (row_idx, col_idx)
+        for (row_idx, col_idx) in targets
+        if col_idx == max_col_idx
+    ]
+
+
 def remove_hp(grid, targets):
     for row_idx, col_idx in targets:
         hp = int(grid[row_idx][col_idx])
         hp -= 1
+        if hp <= 0:
+            hp = '.'
         grid[row_idx][col_idx] = str(hp)
     return grid
 
 
 def move_enemies(grid):
+    # XXX: The "Collision" test case fails without this hacky value.
+    collision = False
     for row_idx, col_idx in get_enemies(grid):
         hp = grid[row_idx][col_idx]
         grid[row_idx][col_idx] = '.'
@@ -103,9 +178,10 @@ def move_enemies(grid):
             raise EnemyWinError
         if grid[row_idx][col_idx] == 'T':
             # Dies if hits tower.
+            collision = True
             continue
         grid[row_idx][col_idx] = hp
-    return grid
+    return (grid, collision)
 
 
 if __name__ == '__main__':
